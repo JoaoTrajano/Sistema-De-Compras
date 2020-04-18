9d4023ea2ac42dd7933f0cfc6b3c667b9bcecbd8
JoaoTrajano <joao.trajanosouza@gmail.com>
2020-04-18 15:02:26 -0300

    alteracao

--- view/menu.php
+++ view/menu.php
@@ -8,7 +8,6 @@ $nome = $_SESSION['nome_usuario'];
 $nivel_usuario = $_SESSION['nivel_usuario'];
 
 if(empty($nome)){
-    session_start();
     unset($_SESSION['nome_usuario']);
     unset($_SESSION['nivel_usuario']);
     session_destroy();
